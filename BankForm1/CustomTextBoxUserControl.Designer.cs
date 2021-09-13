3ca99cb0d1cd8b9f866fa0c627397c8d91872f3d
Rick <rickmedeiros@gmail.com>
2021-09-13 15:11:53 +1200

    CustomTextBox

---
+++ BankForm1/CustomTextBoxUserControl.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace BankForm1
+{
+    partial class CustomTextBoxUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CustomLabel = new System.Windows.Forms.Label();
+            this.CustomTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.CustomLabel);
+            this.panel1.Controls.Add(this.CustomTextBox);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(321, 55);
+            this.panel1.TabIndex = 4;
+            // 
+            // CustomLabel
+            // 
+            this.CustomLabel.AutoSize = true;
+            this.CustomLabel.Location = new System.Drawing.Point(3, 11);
+            this.CustomLabel.Name = "CustomLabel";
+            this.CustomLabel.Size = new System.Drawing.Size(46, 17);
+            this.CustomLabel.TabIndex = 1;
+            this.CustomLabel.Text = "label1";
+            // 
+            // CustomTextBox
+            // 
+            this.CustomTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.CustomTextBox.Location = new System.Drawing.Point(85, 11);
+            this.CustomTextBox.Name = "CustomTextBox";
+            this.CustomTextBox.Size = new System.Drawing.Size(220, 22);
+            this.CustomTextBox.TabIndex = 2;
+            // 
+            // CustomTextBoxUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "CustomTextBoxUserControl";
+            this.Size = new System.Drawing.Size(321, 55);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label CustomLabel;
+        private System.Windows.Forms.TextBox CustomTextBox;
+    }
+}
---
+++ BankForm1/CustomTextBoxUserControl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankForm1
+{
+    public partial class CustomTextBoxUserControl : UserControl
+    {
+        public string LabelText
+        {
+            get
+            {
+                return CustomLabel.Text;
+            }
+            set
+            {
+                CustomLabel.Text = value;
+            }
+        }
+        public string TextInput
+        {
+            get
+            {
+                return CustomTextBox.Text;
+            }
+            set
+            {
+                CustomTextBox.Text = value;
+            }
+        }
+        public CustomTextBoxUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
