b1ad8848f157e948b07ab2294fd8c767194cd097
Rick <rickmedeiros@gmail.com>
2021-09-17 08:47:01 +1200

    File System as data Storage

--- BankForm1/StorageUtilityFunctions.cs
+++ BankForm1/StorageUtilityFunctions.cs
@@ -12,17 +12,22 @@ namespace BankForm1
     public static class StorageUtilityFunctions
     {
         const string MainStorageDir = @"C:\temp";
+        const string CustStorageDir = @"C:\temp2";
+        const string DateStringFormat = "yyyy:MM:dd HH:mm";
+
+        //public static string CustomerName { get; private set; }
 
         static StorageUtilityFunctions()
         {
             if(!CheckMainDirectory())
             {
                 Directory.CreateDirectory(MainStorageDir);
+                Directory.CreateDirectory(CustStorageDir);
             }
         }
         static bool CheckMainDirectory()
         {
-            if(Directory.Exists(MainStorageDir)
+            if(Directory.Exists(MainStorageDir) && Directory.Exists(CustStorageDir))
             {
                 return true;
             }
@@ -47,13 +52,34 @@ namespace BankForm1
                 foreach (Transaction tr in aAccount.ListOfTransactions)
                 {
                     string TransactionLine = tr.TransactionTypeString + "_" + tr.MoneyAmount + "_" + 
-                        tr.TransactionDate.ToString("yyyy:MM:dd HH:mm") + "_" + tr.LocationString;
+                        tr.TransactionDate.ToString(DateStringFormat) + "_" + tr.LocationString;
                     
                     sw.WriteLine(TransactionLine);
                 }
             }
 
-            return false;
+            // File name       CustomerName.dat
+            // Header          CustomerName_BirthDate_Phone
+            // DataLines       AddressLine1\nAddressLine2\nAddressLine3 ...
+              
+            
+            FileInfo FileInfoObject = new FileInfo(CustStorageDir + "\\" + aAccount.CustomerName + ".dat");
+
+            using (StreamWriter sw = FileInfoObject.CreateText())
+            {
+                sw.WriteLine(aAccount.CustomerName + "_" + aAccount.CustomerBirthDate.ToString(DateStringFormat) + "_" +
+                    aAccount.CustomerPhone);
+
+
+                string[] addressLines = aAccount.CustomerAddress.Split(new char[] { '\r', '\n' });
+
+                foreach (string addressLine in addressLines)
+                {
+                    sw.WriteLine(addressLine);
+                }
+
+                return false;
+            }
         }
     }
 }
